dc459217f099662295cbc47a57132f443b95801d
Vlad <vladavramchuk@gmail.com>
2024-09-19 04:34:28 +0300

    Added login page.

---
+++ WebApp/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace WebApp.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        [BindProperty]
+        public CredentialViewModel Credential { get; set; } = new CredentialViewModel();
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid) return Page();
+
+            var result = await _signInManager.PasswordSignInAsync(Credential.Email, Credential.Password, Credential.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError("Login", "You are locked out.");
+                }
+                else
+                {
+                    ModelState.AddModelError("Login", "Failed to login.");
+                }
+
+                return Page();
+            }
+        }
+    }
+
+    public class CredentialViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
