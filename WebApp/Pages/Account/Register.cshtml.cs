e87a5855f72d58f902a8438e6c381bc7c9c06715
Vlad <vladavramchuk@gmail.com>
2024-09-19 03:54:53 +0300

    Created new project to test Identity. Setup registration page and configured identity in origram,cs

---
+++ WebApp/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        [BindProperty]
+        public RegisterViewModel RegisterViewModel { get; set; } = new RegisterViewModel();
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            // Validate Email Address (optional)
+
+
+            // Create the user
+            var user = new IdentityUser
+            {
+                Email = RegisterViewModel.Email,
+                UserName = RegisterViewModel.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, RegisterViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+
+                return Page();
+            }  
+        }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invlaid email address.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(dataType: DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
